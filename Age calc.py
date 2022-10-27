27e8faeaa557f4a16eca45d4abd090889c60ee45
jaysmith13 <janeasmith13@gmail.com>
2022-10-27 00:11:50 -0600

    Update Age calc.py

--- Age calc.py
+++ Age calc.py
@@ -3,4 +3,5 @@ def ageCalculator(y, m, d):
     today = datetime.datetime.now().date()
     dob = datetime.date(y, m, d)
     age = int((today-dob).days / 365.25)
-    
+    print(age)
+ageCalculator(xxxx,xx,xx)
